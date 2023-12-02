a1798eea457bab7e5e1a7cee506f27750586f5fd
Ubaydulloh <132203451+Ubaydulloh-dev@users.noreply.github.com>
2023-12-02 18:12:49 +0500

    Update py-with-dave_rps.py

--- py-with-dave_rps.py
+++ py-with-dave_rps.py
@@ -35,8 +35,15 @@ if player == 1 and computer == 3:
 elif player == 2 and computer == 1:
     print("🎉 You win!!")
 elif player == 3 and computer == 2:
+
+
     print("🎉 You win!!")
 elif player == computer:
     print("😲 Tie game!!")
 else:
     print("🐍 Python wins!!")
+
+
+
+
+# lesson n5 
